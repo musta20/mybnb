943724a0efb163dcd136702fb84258523302fa32
musta20 <must.g20@gmail.com>
2024-04-16 22:56:16 +0200

    adding Listing model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,10 +2,60 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Listing extends Model
 {
-    use HasFactory;
+
+    use HasFactory, HasUlids,SoftDeletes;
+
+
+    protected $fillable = [
+        'title',
+        'description',
+        'address',
+        'city',
+        'country',
+        'latitude',
+        'longitude',
+        'number_of_guests',
+        'number_of_bedrooms',
+        'number_of_bathrooms',
+        'amenities',
+        'price_per_night',
+        'host_id',
+    ];
+
+
+
+
+    /**
+     * Get the host (user) who owns the listing.
+     */
+    public function host(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'host_id');
+    }
+
+    /**
+     * Get the bookings for the listing.
+     */
+    public function bookings(): HasMany
+    {
+        return $this->hasMany(Booking::class);
+    }
+
+    /**
+     * Get the reviews for the listing.
+     */
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(Reviews::class);
+    }
+
 }
