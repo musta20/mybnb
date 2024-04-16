83ebdd1a87b9165674c841075733a2ffd4d28e22
musta20 <must.g20@gmail.com>
2024-04-16 22:46:32 +0200

    adding messages model

--- app/Models/Messages.php
+++ app/Models/Messages.php
@@ -2,10 +2,38 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Messages extends Model
 {
-    use HasFactory;
+    use HasFactory,HasUlids,SoftDeletes;
+
+    protected $fillable = [
+        'sender_id',
+        'recipient_id',
+        'content',
+        'is_read',
+    ];
+
+    /**
+     * Get the user who sent the message.
+     */
+    public function sender(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    /**
+     * Get the user who received the message.
+     */
+    public function recipient(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'recipient_id');
+    }
+
+
 }
