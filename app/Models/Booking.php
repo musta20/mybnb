f907c7b781cfaec75cdd4566532711e7e32b566d
musta20 <must.g20@gmail.com>
2024-04-16 23:06:48 +0200

    adding Booking model

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -2,10 +2,61 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Booking extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes,HasUlids;
+
+
+
+    protected $fillable = [
+        'listing_id',
+        'guest_id',
+        'check_in_date',
+        'check_out_date',
+        'total_price',
+        'status',
+        'special_requests',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'check_in_date' => 'date',
+        'check_out_date' => 'date',
+    ];
+
+  
+    /**
+     * Get the listing associated with the booking.
+     */
+    public function listing(): BelongsTo
+    {
+        return $this->belongsTo(Listing::class);
+    }
+
+    /**
+     * Get the guest (user) who made the booking.
+     */
+    public function guest(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'guest_id');
+    }
+
+    /**
+     * Get the review associated with the booking (optional). 
+     */
+    public function review(): HasOne
+    {
+        return $this->hasOne(Reviews::class);
+    }
 }
