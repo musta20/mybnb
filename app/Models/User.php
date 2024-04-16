ea57de6e500dc4d6ede6ac50a2d1846bd4cf4265
musta20 <must.g20@gmail.com>
2024-04-16 22:36:08 +0200

    adding users model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory,HasFactory,SoftDeletes, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -20,7 +22,19 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'type',
+        'date_of_birth',
+        'phone_number',
+        'profile_picture',
+        'about_me',
+        'languages',
+        'response_time',
+        'response_rate',
     ];
+
+
+
+
 
     /**
      * The attributes that should be hidden for serialization.
@@ -44,4 +58,32 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+    /**
+     * Get the listings owned by the user (host).
+     */
+    public function listings(): HasMany
+    {
+        return $this->hasMany(Listing::class, 'host_id');
+    }
+
+    /**
+     * Get the bookings made by the user (guest).
+     */
+    public function bookings(): HasMany
+    {
+        return $this->hasMany(Booking::class, 'guest_id');
+    }
+
+    /**
+     * Get the reviews written by the user (guest). 
+     */
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(Review::class, 'guest_id');
+    }
+
+
+    
 }
